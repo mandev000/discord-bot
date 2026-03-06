1bc81a6c62582b35bb1ab6028bc8e0ab7ce5bde5
mandev000 <hamo10101961@gmail.com>
2026-03-06 15:23:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
 });
 
 client.once("ready", () => {
-  console.log("Bot online");
+  console.log("Bot đã online!");
 });
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
 
   if (message.content.toLowerCase() === "help") {
-    message.reply("📜 Commands:\nhelp\nping\nngocay");
+    message.reply("📜 Lệnh:\nhelp\nping\nngocay");
   }
 
   if (message.content.toLowerCase() === "ping") {
@@ -24,20 +28,6 @@ client.on("messageCreate", (message) => {
       content: "Ngô Cay 🌽",
       files: ["https://png.pngtree.com/png-vector/20250918/ourmid/pngtree-hot-steamed-corn-on-cob-served-with-butter-and-masala-seasoning-png-image_17496796.webp"]
     });
-  }
-
-});
-
-  if (message.content.toLowerCase() === "help") {
-    message.reply("📜 Commands:\nhelp\nping\ninfo");
-  }
-
-  if (message.content.toLowerCase() === "ping") {
-    message.reply("🏓 Pong!");
-  }
-
-  if (message.content.toLowerCase() === "info") {
-    message.reply("🤖 Bot đang chạy trên Railway!");
   }
 });
 
